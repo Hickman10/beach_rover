b2ae5002210030de56b586a3c3e2f6982b3a1cce
Hickman10 <hickmanmohammed@gmail.com>
2024-01-15 18:26:49 +0000

    updated code to lift and release the sorting section

--- beach_rover.ino
+++ beach_rover.ino
@@ -1,14 +1,15 @@
 const int LeftMotorForward = 2;
 const int LeftMotorBackward = 3;
-const int RightMotorForward = 4;
-const int RightMotorBackward = 5;
+const int LiftUp = 4;
+const int LiftDown = 5;
 
+char command;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  pinMode(RightMotorBackward, OUTPUT);
-  pinMode(RightMotorForward,OUTPUT);
+  pinMode(LiftDown, OUTPUT);
+  pinMode(LiftUp,OUTPUT);
 
   pinMode(LeftMotorBackward, OUTPUT);
   pinMode(LeftMotorForward,OUTPUT);
@@ -18,13 +19,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available() > 0){ 
       command = Serial.read(); 
-      Stop();
+      stop();
       switch(command){
       case 'F':  
-        forward();
+        move_forward();
         break;
       case 'B':  
-        back();
+        move_backward();
         break;
       case 'L':  
         lift_bin();
@@ -33,36 +34,47 @@ void loop() {
         lower_bin();
         break;
       }
+  }
 }
 
 void move_forward(){
-  digitalWrite(RightMotorForward, HIGH);
-  digitalWrite(RightMotorBackward, LOW);
-
   //delay(2000);
 
   digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(LeftMotorBackward, LOW);;
-  delay(5000);
+  digitalWrite(LeftMotorBackward, LOW);
+  delay(500);
 }
 
 void move_backward(){
-  digitalWrite(RightMotorForward, LOW);
-  digitalWrite(RightMotorBackward, HIGH);
+  
 
   //delay(2000);
 
   digitalWrite(LeftMotorForward, LOW);
   digitalWrite(LeftMotorBackward, HIGH);;
-  delay(5000);
+  delay(500);
+}
+
+void stop(){
+  //delay(2000);
+
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);;
+  delay(500);
 }
 
 void lift_bin(){
   // lift the trash for sorting using 
+  digitalWrite(LiftUp, HIGH);
+  digitalWrite(LiftDown, LOW);
+  delay();
 }
 
 void lower_bin(){
   // lower bin to collect incomming trash
+  digitalWrite(LiftUp, LOW);
+  digitalWrite(LiftDown, HIGH);\
+  delay();
 }
 
 void shake(){
