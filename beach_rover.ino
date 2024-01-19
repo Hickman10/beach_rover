a5572eacc61d8e3add943ecf02ba7ea7631533d8
Hickman10 <hickmanmohammed@gmail.com>
2024-01-19 20:34:06 +0000

    add the shaking

--- beach_rover.ino
+++ beach_rover.ino
@@ -3,6 +3,10 @@ const int LeftMotorBackward = 3;
 const int LiftUp = 4;
 const int LiftDown = 5;
 
+const int dirPin = 8;
+const int stepPin = 9;
+const int stepsPerRevolution = 10;
+
 char command;
 void setup() {
   // put your setup code here, to run once:
@@ -13,6 +17,8 @@ void setup() {
 
   pinMode(LeftMotorBackward, OUTPUT);
   pinMode(LeftMotorForward,OUTPUT);
+  pinMode(stepPin, OUTPUT);
+	pinMode(dirPin, OUTPUT);
 }
 
 void loop() {
@@ -29,109 +35,17 @@ void loop() {
       case 'B':  
         move_backward();
         break;
-      case 'L':  
+      case 'L':
+        stop(); 
         lift_bin();
         break;
       case 'R':
         lower_bin();
         break;
       }
+      Serial.println("hene");
   }
-  
-   int maxval = 0;
- 
-  //perform measurement
-  long unsigned int sum = 0;
-  for (int imeas = 0; imeas < nmeas + 2; imeas++) {
-    //reset the capacitor
-    pinMode(pin_cap, OUTPUT);
-    digitalWrite(pin_cap, LOW);
-    delayMicroseconds(20);
-    pinMode(pin_cap, INPUT);
-    //apply pulses
-    for (int ipulse = 0; ipulse < npulse; ipulse++) {
-      digitalWrite(pin_pulse, HIGH); //takes 3.5 microseconds
-      delayMicroseconds(30);
-      digitalWrite(pin_pulse, LOW); //takes 3.5 microseconds
-      delayMicroseconds(30);
-    }
-    //read the charge on the capacitor
-    int val = analogRead(pin_cap); //takes 13x8=104 microseconds
-    minval = min(val, minval);
-    maxval = max(val, maxval);
-    sum += val;
- 
-    //determine if LEDs should be on or off
-    long unsigned int timestamp = millis();
-    
-    if (timestamp < prev_flash +12) {
-      if (diff > 0)ledstat = 2;
-      if (diff < 0)ledstat = 1;
-    }
-    if (timestamp > prev_flash + flash_period) {
-      if (diff > 0)ledstat = 2;
-      if (diff < 0)ledstat = 1;
-      prev_flash = timestamp;
-    }
-    if (flash_period > 1000)ledstat = 0;
- 
-    
-  }
- 
-  //subtract minimum and maximum value to remove spikes
-  sum -= minval; sum -= maxval;
- 
-  //process
-  if (sumsum == 0) sumsum = sum << 6; //set sumsum to expected value
-  long int avgsum = (sumsum + 32) >> 6;
-  diff = sum - avgsum;
-  if (abs(diff)<avgsum >> 10) {   //adjust for small changes
-    sumsum = sumsum + sum - avgsum;
-    skip = 0;
-  } else {
-    skip++;
-  }
-  if (skip > 64) {  // break off in case of prolonged skipping
-    sumsum = sum << 6;
-    skip = 0;
-  }
- 
-  // one permille change = 2 ticks/s
-  if (diff == 0) flash_period = 1000000;
-  else flash_period = avgsum / (2 * abs(diff));
-  delay(4000);
-  //switch the LEDs to this setting
-  if (ledstat == 0) {
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("low");
-  }
-  if (ledstat == 1) {
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("low1");
-  }
-  if (ledstat == 2) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("high");
-    
-    for (pos = 90; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15 ms for the servo to reach the position
-    }
-    
-    Serial.println("hjjej");
-    for (pos = 180; pos >= 90; pos -= 1) { // goes from 180 degrees to 0 degrees
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15 ms for the servo to reach the position
-    }
-    delay(2000);
-    
-  }
-    
-  Serial.println("end");
-  Serial.print(ledstat);
-  ledstat == 0; int minval = 2000;
-
+  shake();
 }
 
 void move_forward(){
@@ -170,7 +84,8 @@ void lift_bin(){
   delay(500);
   digitalWrite(LiftUp, LOW);
   digitalWrite(LiftDown, LOW);
-  delay(800);
+  delay(500);
+  Serial.println("lifti");
 }
 
 void lower_bin(){
@@ -180,8 +95,36 @@ void lower_bin(){
   delay(500);
   digitalWrite(LiftUp, LOW);\
   digitalWrite(LiftDown, LOW);
+  Serial.println("return");
 }
 
 void shake(){
   // shake to let the sand fall
+  Serial.println("gelll");
+  
+	// Set motor direction clockwise
+	digitalWrite(dirPin, HIGH);
+
+	// Spin motor slowly
+	for(int x = 0; x < stepsPerRevolution; x++)
+	{
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(500);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(500);
+	}
+	delay(1000); // Wait a second
+	
+	// Set motor direction counterclockwise
+	digitalWrite(dirPin, LOW);
+
+	// Spin motor quickly
+	for(int x = 0; x < stepsPerRevolution; x++)
+	{
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(500);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(500);
+	}
+	delay(1000); // Wait a second
 }
